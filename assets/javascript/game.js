c0e847d6d3e5046d64ab4991697cdbc2347da7bb
Travis <travishill1@gmail.com>
2019-01-14 01:06:32 -0600

    functional game, win and lose needs work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,18 @@
 
+   var audioPunch = document.createElement("audio");
+   audioPunch.setAttribute("src", "assets/Punch.mp3");
+
+   var audioTheme = document.createElement("audio");
+    audioTheme.setAttribute("src", "assets/themesong.mp3");
+
+    // Theme Button
+    $(".theme-button").on("click", function() {
+      audioTheme.play();
+    });
+    $(".pause-button").on("click", function() {
+      audioTheme.pause();
+    });
+
  // We create a constructor as a template for all character objects to call upon during game initialization.
 class Hero {
 	constructor(name, health, baseAttack) {
@@ -14,9 +28,11 @@ class Hero {
         let imageURL = "assets/images/" + this.name + ".jpg";
         let card = $("<div>");
         card.addClass("card");
+        card.addClass("col-3");
         card.append("<p class='nameText'>" + this.name + "</p>");
         card.append("<img class ='cardImage' src='" + imageURL + "'>");
         card.append("<p class='hp'>" + this.health + "</p>");
+        card.appendTo("#lobby-area");
 
         this.card = card;
     }
@@ -52,9 +68,8 @@ const game = {
     needDefender : true,
     needPlayer : true,
 
-
     // determine if game initiation is outside or within the game object.
-initiate : function ( ) {
+initiate : function () {
 
     game.characters = [
         new Hero("Goku", 150, 15),
@@ -66,11 +81,12 @@ initiate : function ( ) {
     game.needDefender = true;
     game.needPlayer = true;
     game.addListeners();
-
+    $("#message").empty();
     
     $("#attack-button").on("click", function() {
         if (!game.needPlayer & !game.needDefender){
             game.round();
+            audioPunch.play();
         }
     });
 },
@@ -78,7 +94,6 @@ initiate : function ( ) {
 addListeners : function(){
     for (let i in game.characters){
         character = game.characters[i];
-        // character.card.appendTo("#lobby-area");
         character.card.on("click", function(){
             if (game.needPlayer){
                 game.addPlayer(game.characters[i]);
@@ -87,54 +102,60 @@ addListeners : function(){
                 game.addDefender(game.characters[i]);
             }
         });
-        };
+    }
 },
 
-addPlayer : function ( ) {
+addPlayer : function(character) {
     character.card.addClass("player");
-    // character.card.removeClass("inLobby");
     character.card.remove();
     $("#player-area").append(character.card);
     game.addListeners();
-    // character.isPlayer = true;
     game.player = character;
     game.needPlayer = false;
+    $(".characterSelection").remove();
+    character.card.removeClass("col-3");
+    character.card.addClass("col-10");
 },
 
-addDefender : function ( ) {
-    character.card.addClass("defender");
-    character.card.remove();
-    // character.card.removeClass("inLobby");
-    $("#defender-area").append(character.card);
-    // character.isDefender = true;
-    game.defender = character;
-    game.needDefender = false;
-},
-
-// make sure .remove here doesn't prevent the next line of code adding to the enemy area. = OK (saved by memory)
-moveEnemies : function ( ) {
+moveEnemies : function() {
     for (let character of game.characters){
         if (game.player != character){
             character.card.addClass("enemy");
             character.card.remove();
-            game.addListeners();
-            // character.card.removeClass("inLobby");
             $("#enemy-area").append(character.card);
-
+            game.addListeners();
         }
-    }
-    
+    }   
 },
 
-round : function ( ) {
-    player.attack(defender);
-    player.powerUp();
-    defender.attack(player);
-    if (player.checkDead()) {
-        game.Lose ()
-    };
-    if (defender.checkDead()) {
+addDefender : function(character){
+    if (character != game.player){
+    character.card.addClass("defender");
+    game.defender = character;
+    character.card.remove();
+    $("#defender-area").append(character.card);
+    game.needDefender = false;
+    game.addListeners();
+    character.card.removeClass("col-3");
+    character.card.addClass("col-10");
+    }
+},
+
+// make sure .remove here doesn't prevent the next line of code adding to the enemy area. = OK (saved by memory)
+
+
+round : function(){
+    game.player.attack(game.defender);
+    game.player.powerUp();
+    game.defender.attack(game.player);
+    game.player.card.find(".hp").text(game.player.health);
+    game.defender.card.find(".hp").text(game.defender.health);
+    // game.checkBattleEnd();
+    if (game.defender.checkDead()) {
         game.battleWin ()
+    };
+    if (game.player.checkDead()) {
+        game.loseGame ()
     };
 },
 
@@ -152,58 +173,44 @@ round : function ( ) {
 
 
 // need game win, battle win, and battle loss.  no game loss because === battle loss.
-battleWin : function ( ) {
-game.defender.card.hide( );
-game.defender = none;
-needDefender = true;
+battleWin : function(){
+game.defender.card.remove();
+game.defender = null;
+game.needDefender = true;
 },
 
-endGame : function ( ) {
+createButton : function(){
+    let button = $("<button>");
+    button.text("New Game");
+    button.addClass("btn btn-dark btn-lg");
+    button.attr('id', 'reset-button');
+    button.on("click", function(){
+        game.resetGame();
+        game.initiate();
+    })
+    return button;
+},
+
+loseGame : function(){
+    $("#message").append("<h2>You Lose!</h2>")
+    $("#message").append(game.createButton());
+},
+
+winGame : function(){
+    $("#message").append("<h2>You Win!</h2>")
+    $("#message").append(game.createButton());
+},
+
+resetGame : function(){
     for (let character of game.characters){
         character.card.off("click");
-        };
-    $("#attack-button").off("click")
-    },
+    }
 
-winGame : function ( ) {
+    $("#attack-button").off("click");
+    game.player.card.remove();
+    game.player = null;
+    $("#reset-button").remove();
+}
+}
 
-},
-
-loseGame : function ( ) {
-
-},
-
-attack : function ( ) {
-    $("#attack-button").on("click", function() {
-        if (defender.checkDead()) {
-            game.initiate();
-        }
-        else {
-            game.round();
-        }
-    });
-},
-
-// 7. Create an "on-click" event attached to the ".letter-button" class.
-// $(".letter-button").on("click", function() {
-
-//     // Inside the on-click event...
-
-//     // 8. Create a variable called "fridgeMagnet" and set the variable equal to a new div.
-//     var fridgeMagnet = $("<div>");
-
-//     // 9. Give each "fridgeMagnet" the following classes: "letter fridge-color".
-//     fridgeMagnet.addClass("letter fridge-color");
-
-//     // 10. Then chain the following code onto the "fridgeMagnet" variable: .text($(this).attr("data-letter"))
-//     // attr acts as both a setter and a getter for attributes depending on whether we supply one argument or two
-//     // NOTE: There IS a $(data) jQuery method, but it doesn't do what you'd expect. So just use attr.
-//     fridgeMagnet.text($(this).attr("data-letter"));
-
-//     // 11. Lastly append the fridgeMagnet variable to the "#display" div (provided);
-//     // Again you can see we use that find, and once its found we append the item
-//     $("#display").append(fridgeMagnet);
-
-//   }),
-};
 game.initiate();
